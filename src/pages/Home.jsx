42ef0c45cd8f79b49726f8832aae0d13a3abce82
DerickGS <derick@galdinoo.com>
2024-09-23 19:45:53 -0300

    vite post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header"
-import Footer from "../components/footer"
+import Footer from "../components/Footer"
 // import { Link } from "react-router-dom"
 // import Logo from "../assets/_iconLogo.png"
 // import styles from "./Home.module.css"
