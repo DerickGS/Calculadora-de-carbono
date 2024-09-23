c3f235155486faed2d6cba567b075fe7180904d5
DerickGS <derick@galdinoo.com>
2024-09-23 19:47:47 -0300

    vite post correction

--- src/pages/InformativePage.jsx
+++ src/pages/InformativePage.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header"
-import Footer from "../components/footer"
+import Footer from "../components/Footer"
 
 import Correct from "../img/correct.png"
 import Wrong from "../img/wrong.png"
