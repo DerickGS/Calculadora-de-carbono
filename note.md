08cbb3d99f2159408a2775895e5e2644c7e7f9a2
DerickGS <derick@galdinoo.com>
2024-09-27 16:41:17 -0300

    form contact page

--- note.md
+++ note.md
@@ -2,11 +2,15 @@ respansividade header ✅
 respansividade footer ✅
 
 paginas
+    Home
     Calculadora
         nao pode retornar sem estra com a tabela completa
+         R$ 50.000 - R$ 100.000  valores 500 toneladas
+         hectares de areas recuperadas 
     Curiosidade
     Equipe ✅
     Contato!!!!!!!!!!
+        https://formsubmit.co/
 
 respansividade
     Calculadora ✅
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -10,6 +10,16 @@ function ContactPage() {
             <Header/>
             <body>
                 <h1 className={styles}>Pagina de contato</h1>
+                <div>
+                    <form className={styles._formEmail} action="https://formsubmit.co/derick@eaportal.org" method="POST">
+                        <input type="text" name="first name" placeholder="Nome:" required></input>
+                        <input type="text" name="lastname" placeholder="Sobrenome:" required></input>
+                        <input type="email" name="email" placeholder="Email Address" required></input>
+                        <input id="phone" type="tel" name="numeroContato" placeholder="Número de Contato:" required></input>
+                        <textarea name="message" placeholder="Mensagem:" required></textarea>
+                        <button>Enviar</button>
+                    </form>
+                </div>
             </body>
             <Footer/>
         </main>
