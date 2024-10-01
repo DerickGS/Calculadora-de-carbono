42a8a197c3af79d9c19fb95864a7464ea489cae6
DerickGS <derick@galdinoo.com>
2024-10-01 16:49:14 -0300

    contact page responsive form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -12,9 +12,11 @@ function ContactPage() {
                 <h1 className={styles}>Pagina de contato</h1>
                 <div>
                     <form className={styles._formEmail} action="https://formsubmit.co/derick@eaportal.org" method="POST">
-                        <input type="text" name="first name" placeholder="Nome:" required></input>
-                        <input type="text" name="lastname" placeholder="Sobrenome:" required></input>
-                        <input type="email" name="email" placeholder="Email Address" required></input>
+                        <div className={styles._inputName}>
+                            <input type="text" name="first name" placeholder="Nome:" required></input>
+                            <input type="text" name="lastname" placeholder="Sobrenome:" required></input>
+                        </div>
+                        <input type="email" name="email" placeholder="Email:" required></input>
                         <input id="phone" type="tel" name="numeroContato" placeholder="Número de Contato:" required></input>
                         <textarea name="message" placeholder="Mensagem:" required></textarea>
                         <button>Enviar</button>
