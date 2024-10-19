2c7165366b2bb6073228902261372ce6d8806483
DerickGS <derick@galdinoo.com>
2024-10-19 19:04:02 -0300

    change img page curiosity

--- src/pages/CuriosityPage.jsx
+++ src/pages/CuriosityPage.jsx
@@ -7,7 +7,7 @@ import styles from "./CuriosityPage.module.css"
 
 import Calculo1 from "../img/calculo1.png"
 import Calculo2 from "../img/calculo2.png"
-import Calculo3 from "../img/calculo2.png"
+import Calculo3 from "../img/calculo3.png"
 import CreditoDeCarbono from "../img/Credito-de-carbono.png"
 
 function CuriosityPage() {
